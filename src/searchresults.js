88fae1c89d1bdd41b3c6c89d65c97e77acb9a61b
nwbc <nwbc@gmx.com>
2016-02-01 16:32:07 +0100

    search stuff

--- src/searchresults.js
+++ src/searchresults.js
@@ -1,36 +1,37 @@
 var React = require('react');
 var Results = require('../data.json');
+var HomeCard = require('./homecard.js').HomeCard;
 
 var SearchResults = React.createClass({
   render: function() {
     var resultList = Results.map(function(estate) {
       return (
-        <Estate estateInfo = {estate} />
+        <li><HomeCard home = {estate} /></li>
       );
     });
     return(
-      <div className="container">
-        <ul className="results">{resultList}</ul>
+      <div className="dark-container">
+        <ul className="row results">{resultList}</ul>
       </div>
     );
   }
 });
 
-var Estate = React.createClass({
-  render: function() {
-    return (
-      <li className="row">
-        <div className="col-xs-2">
-          <img src={this.props.estateInfo.image}></img>
-        </div>
-        <div className="col-xs-10">
-          <p>Location: {this.props.estateInfo.location}</p>
-          <p>Price: {this.props.estateInfo.price}</p>
-          <a href={this.props.estateInfo.url}>More</a>
-        </div>
-      </li>
-    );
-  }
-});
+// var Estate = React.createClass({
+//   render: function() {
+//     return (
+//       <li className="row">
+//         <div className="col-xs-2">
+//           <img src={this.props.estateInfo.image[0]}></img>
+//         </div>
+//         <div className="col-xs-10">
+//           <p>Location: {this.props.estateInfo.location}</p>
+//           <p>Price: {this.props.estateInfo.price}</p>
+//           <a href={this.props.estateInfo.url}>More</a>
+//         </div>
+//       </li>
+//     );
+//   }
+// });
 
 module.exports.SearchResults = SearchResults;
