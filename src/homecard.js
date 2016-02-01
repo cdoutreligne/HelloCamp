c4f34a864514219980682a1586a7b1659fcef63f
Eminanza <michaeltihon@gmail.com>
2016-02-01 13:35:49 +0100

    Daily

--- src/homecard.js
+++ src/homecard.js
@@ -3,19 +3,8 @@ var React = require('react');
 var HomeCard = React.createClass({
   render: function() {
     return (
-        <div id="page-content-wrapper" className="main-container">
-            <div className="container-fluid navbar-container">
-            <nav className="navbar navbar-inverse full-transparent">
-              <div className="navbar-header">
-                <a className="icon-menu navbar-toggle menu-button" id="sidebar-toggle">&nbsp;MENU</a>
-              </div>
-              <div>
-                <ul className="nav navbar-nav pull-right">
-                  <li><a href="#" className="navbar-link">FR</a></li>
-                </ul>
-              </div>
-            </nav>
-            </div>
+        <div className="dark-container">
+
         </div>
     );
   }
