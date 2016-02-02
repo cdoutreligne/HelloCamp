3a8ae0c2aad2f7471b4bd3a1ce51fceabd98f301
Jonas Coenen <jonas.coenen005@gmail.com>
2016-02-02 13:39:43 +0100

    implementing of wishlist

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,9 +1,35 @@
 var React = require('react');
+var Results = require('../data.json');
+var HomeCard = require('./homecard.js').HomeCard;
 
 var WishList = React.createClass({
   render: function(){
+
+
+    var wishlistIds = this.props.wishlistIds;
+
+    var filterList = function(){
+      var tempArray = [];
+      for (var i = Results.length - 1; i >= 0; i--) {
+        for (var j = wishlistIds.length - 1; j >= 0; j--) {
+          if(wishlistIds[j] === Results[i].id) {
+            tempArray.push(Results[i]);
+          }
+        };
+      };
+      return tempArray;
+    }
+
+    var resultList = filterList().map(function(estate) {
+      return (
+        <li><HomeCard home = {estate} /></li>
+      );
+    });
+
     return(
-      <h1>Placeholder wishlist</h1>
+      <div className="dark-container">
+        <ul className="row results">{resultList}</ul>
+      </div>
     );
   }
 });
