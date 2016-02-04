36644c5aeb2e4ee98866ef3bd61b94ab32cfffc2
Jonas Coenen <jonas.coenen005@gmail.com>
2016-02-04 13:11:44 +0100

    made simulation dynamically

--- src/simulation.js
+++ src/simulation.js
@@ -1,9 +1,31 @@
 var React = require('react');
 
 var Simulation = React.createClass({
+  prettifyValue: function(number){
+    number = number.toFixed(2) + '';
+    x = number.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? ',' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    }
+    return x1 + x2 + " €";
+  },
+
+  priceHome: function(){
+    return this.prettifyValue(parseFloat(this.props.home.price));
+  },
+
+  totalCostPrice: function(){
+    return this.prettifyValue(parseFloat(this.props.home.price) * 1.32);
+  },
+
+  pricePerMonth: function(){
+    return this.prettifyValue((parseFloat(this.props.home.price) * 1.32)/240);
+  },
+
   render: function(){
-  var home = this.props.home;
-  console.log(home);
     return(
       <div className="dark-container">
         <div className="simulationResult">
@@ -13,7 +35,7 @@ var Simulation = React.createClass({
               <tr>
                 <td className="halfRow">
                   <h3>Montant du prêt</h3>
-                  <p>home.price</p>
+                  <p>{this.priceHome()}</p>
                 </td>
                 <td className="halfRow">
                   <h3>Durée</h3>
@@ -24,7 +46,7 @@ var Simulation = React.createClass({
               <tr>
                 <td colSpan="2">
                   <h3>Mensualité</h3>
-                  <p>1.642,25 €</p>
+                  <p>{this.pricePerMonth()}</p>
                 </td>
               </tr>
 
@@ -37,7 +59,7 @@ var Simulation = React.createClass({
 
               <tr>
                 <td colSpan="2">
-                  <h3 id="inlineCol">Coût du crédit après 20 ans :</h3><p id="inlineCol2">394.140,00 €</p>
+                  <h3 id="inlineCol">Coût du crédit après 20 ans :</h3><p id="inlineCol2">{this.totalCostPrice()}</p>
                 </td>
               </tr>
             </tbody>
