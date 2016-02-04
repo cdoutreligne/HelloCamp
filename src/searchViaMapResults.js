8d68ec19c4c250c6eef29527d29437f1a58cd826
Eminanza <michaeltihon@gmail.com>
2016-02-04 14:04:29 +0100

    Daily

--- src/searchViaMapResults.js
+++ src/searchViaMapResults.js
@@ -11,11 +11,33 @@ var SearchViaMapResults = React.createClass({
      },
  
     render: function() {
+        function distance(lat1, lon1, lat2, lon2, unit) {
+            var radlat1 = Math.PI * lat1/180
+            var radlat2 = Math.PI * lat2/180
+            var theta = lon1-lon2
+            var radtheta = Math.PI * theta/180
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            dist = Math.acos(dist)
+            dist = dist * 180/Math.PI
+            dist = dist * 60 * 1.1515
+            if (unit=="K") { dist = dist * 1.609344 }
+            if (unit=="N") { dist = dist * 0.8684 }
+            return dist
+        }
+
             // "coordinates": [50.7747465, 4.244960499999999],
     var latitudeA = 50.847700;
     var longitudeA = 4.360362;
     var latitudeB =  50.835926;
     var longitudeB = 4.414975;
+
+    console.log(distance(latitudeA,longitudeA,latitudeB,longitudeB,"K"));
+    // if (distance(latitudeA,longitudeA,latitudeB,longitudeB,"K")>0,5) {
+    //   console.log(distance(latitudeA,longitudeA,latitudeB,longitudeB,"K"));
+    // } else {
+    //   console.log(distance(latitudeA,longitudeA,latitudeB,longitudeB,"K"));
+    // }
+
 
 
     // var displayMap = false;
@@ -27,11 +49,13 @@ var SearchViaMapResults = React.createClass({
 
     // var marker = {lat: latitude, lng: longitude};
     var distanceAB = 6371 * Math.acos((Math.cos(latitudeA) * Math.cos(latitudeB) * Math.cos(longitudeB-longitudeA)) + (Math.sin(latitudeA) * Math.sin(latitudeB)));
-console.log("distanceAB: "+distanceAB)
+    // console.log("distanceAB: "+distanceAB)
     var marker = {lat: latitudeA, lng: longitudeA}
     var mapMarkers = <Marker position={marker} />;
     var googleMap = "";
     googleMap=<section style={{height: "366px"}}><GoogleMapLoader containerElement={<div style={{ height: "100%" }} />} googleMapElement={<GoogleMap defaultZoom={11} defaultCenter={marker}>{ mapMarkers }</GoogleMap>}/></section>;
+
+    
 
         return (
 
